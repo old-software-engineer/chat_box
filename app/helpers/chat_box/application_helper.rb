40e344f2e34d7966b11d07c5c5c0c94027c3d8ce
ravi kumar <ravikumar@codegaragetech.com>
2020-06-15 10:04:47 +0530

    adding title option

--- app/helpers/chat_box/application_helper.rb
+++ app/helpers/chat_box/application_helper.rb
@@ -1,10 +1,11 @@
 module ChatBox
   module ApplicationHelper
-    def chat_section
+    def chat_section(option={})
+    	option[:title] = option[:title] || "Chat List"
       if user_signed_in?
         users = current_user.chat_list
         conversations = session[:conversations].nil? || session[:conversations].blank? ? [] : Conversation.includes(:recipient, :messages).where(id: session[:conversations])
-        render partial: "chat_box/user/user_list",locals:{users: users,conversations: conversations}
+        render partial: "chat_box/user/user_list",locals:{users: users,conversations: conversations,option: option}
       end
     end
   end
